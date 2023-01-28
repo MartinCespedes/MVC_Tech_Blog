09409156c0d76bef7e3e76e1999169a888b17f8c
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-28 11:51:21 -0500

    Added postRoutes.js and fixed connections

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const userRoutes = require("./userRoutes.js");
-const postRoutes = require("./postRoutes.js");
+const postRoutes = require("../api/");
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
